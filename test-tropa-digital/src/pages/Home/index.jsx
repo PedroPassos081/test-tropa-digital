f0c491b900d807107e00473134c1a68830b45640
Pedro Passos <pedro.passos081@gmail.com>
2025-06-09 21:29:25 -0300

    Iniciando a Home principal

--- test-tropa-digital/src/pages/Home/index.jsx
+++ test-tropa-digital/src/pages/Home/index.jsx
@@ -1,7 +1,34 @@
+import Paragraph from "../../components/Paragraph";
 import Sidebar from "../../components/Sidebar";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const ConteudoPrincipal = styled.div`
+  padding: 30px;
+  overflow-y: auto;
+`;
+
+const Span = styled.span`
+  color: #000;
+  font-weight: bold;
+`;
 
 function Home() {
-  return <Sidebar />;
+  return (
+    <Container>
+      <Sidebar />
+      <ConteudoPrincipal>
+        <Paragraph>
+          Bem vindo de volta, <Span>Kaique Steck</Span>
+        </Paragraph>
+        <Span>Todos eventos</Span>
+      </ConteudoPrincipal>
+    </Container>
+  );
 }
 
 export default Home;
